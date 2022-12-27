89606ab54bfa445b877ebb32ffdd263c8aeddce9
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-27 22:19:08 +0530

    First Commit

---
+++ 01BASICS/variables.js
@@ -0,0 +1,27 @@
+// Variable : It is space in the memory that you say is hey you reserved it for me I'm gonnamput something into it
+let initialname = 'Sowmi'
+console.log(initialname)
+
+let score ='100'
+// let scoreshouldbe = 103
+let bonus='20'
+let totalscore = score + bonus
+// string concatenation will takes place
+console.log(totalscore)
+//Anything inside the quote'' "" it is string not a number
+
+let mark =100
+let grace=20
+let gracemark = mark + grace
+console.log(gracemark)
+
+
+let firstName ='Rini'
+let lastName ='Adhithi'
+let fullName = firstName + '**' + lastName
+console.log(firstName +' '+ lastName)
+console.log(fullName)
+
+// const it is fixed but let can change
+
+const secondName ='sowmi'
