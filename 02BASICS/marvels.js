647f8721211b1c88d6849dacc9b23f0798185f69
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-29 19:13:45 +0530

    Array Concept

---
+++ 02BASICS/marvels.js
@@ -0,0 +1,18 @@
+// Datatypes - Array
+
+const superHeroes =['Iron Man', 'Spider Man', 'Super Man']
+
+console.log(superHeroes);
+console.log(superHeroes[0]);
+console.log(superHeroes[1]);
+console.log(superHeroes[2]);
+
+// We need to calculate the length of the Array
+// For N number of elements we can't able to predict the last element
+console.log(superHeroes[superHeroes.length-2]);
+console.log(`We have ${superHeroes.length} Super Heroes`);
+
+
+// const array =['Sowmi','Rini','Lavi','Bhuvi']
+// console.log(array[array.length -1]);
+// console.log(`Hey Hello We are ${array.length}`);
