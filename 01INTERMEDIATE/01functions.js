d84e752e0a59f5013204e944b48f08bc8c9192b8
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-29 20:13:12 +0530

    Functions

---
+++ 01INTERMEDIATE/01functions.js
@@ -0,0 +1,22 @@
+// Define a function
+let sayHello = function(name){
+    console.log(`Greeting message for ${name}`);
+    console.log(`Hey ${name}! Welcome to the New World!!`);
+}
+// sayHello('Sowmi')
+// sayHello('Rini')
+
+
+let fullNameMaker = function (firstname, lastname) {
+    console.log(`Welcome to the World ${firstname}`)
+    console.log(`Happy to have you here ${firstname} ${lastname}`);
+}
+// fullNameMaker('Rini', 'Adhithi')
+
+let  myAdder=function(num1,num2){
+    let added = num1 + num2
+    return added
+}
+console.log(myAdder(2367,25376));
+// let result =myAdder(2367,25376)
+// console.log(result);
