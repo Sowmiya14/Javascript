c46ec8fb5c3af3fdc3dc1cb3b9ff9e5838ef7dd2
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-29 09:48:26 +0530

    Fahrenheit to Celsius Conversion

---
+++ 01BASICS/celsius.js
@@ -0,0 +1,18 @@
+var score=100
+score=130
+const bonus=50
+var finalScore=(score + bonus)
+var multipliedScore=(score + bonus)*1.44
+console.log(finalScore)
+console.log(multipliedScore)
+var complex=10+25*20/2
+console.log(complex)
+// It should give under the parantheses for perfect calculation
+var complexx=((10+25)*20)/2
+console.log(complexx)
+
+//Fahrenheit to Celsius Conversion
+// °C = [(°F-32)×5]/9
+let tempInFahrenheit = 100
+let celsius = (tempInFahrenheit-32)*5/9
+console.log(celsius)
