b4829a3be5c351c292c42aaf8fed1ad9484c9cac
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-29 10:09:40 +0530

    Null & Boolean

---
+++ 01BASICS/nullandbool.js
@@ -0,0 +1,17 @@
+// Temp is coming from website API
+var temp=0
+// Null and Undefined is completely different
+console.log("Current Temperature is " + temp)
+
+// Grade System
+// 10 = Excellent
+// 8 = Good
+// 6 = Work Harder
+// 4 = Poor
+// 2 = Worst
+// 0 = Fail
+
+// Boolean Operations - True/False
+let actualMark = 8
+let myGrade = (actualMark < 10)
+console.log(myGrade)
