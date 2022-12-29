cc9b67c5b86b41fba6c1c13d9fa01ebc25b5eff6
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-29 11:33:55 +0530

    Grading System

---
+++ 02BASICS/learnifElse.js
@@ -0,0 +1,53 @@
+// if (true) {
+//     console.log("I am inside if Block")
+//     console.log("I am still inside if Block")
+// }
+
+// if (false) {
+//     console.log("I am inside if Block")  
+// } else if(true){
+//     console.log("I am inside the ELSE IF Block")
+// } else {
+//     console.log("Notice this Part")
+// }
+
+// if (false) {
+//     console.log("I am inside if Block")  
+// } else if(false){
+//     console.log("I am inside the ELSE IF Block")
+// } else {
+//     console.log("Notice this Part")
+// }
+
+// Learn Code Online website
+        // var whoIsHere = ''
+        // if (whoIsHere === 'user') {
+        //     console.log('Greeting message for user')
+        //     console.log('Allow Access to View All Courses')
+        // }else if(whoIsHere === 'teacher'){
+        //     console.log('Greeting message for teacher')
+        //     console.log('Allow Access to his Courses')
+        // }else{
+        //     console.log('MESSAGE: Please Verify Your Email')
+        //     console.log('Send User Email for Verification')
+        // }
+
+// Assignment : Problem based on Grading System
+// Student Marks
+//  10 - Amazing
+//  5 - Good
+//  3 - Poor
+//  0 - Fail
+
+var studentMarks = ''
+if (studentMarks === 10) {
+    console.log('You are Amazing')
+} else if(studentMarks === 5) {
+    console.log('You are Good')
+} else if(studentMarks === 3){
+    console.log('You are Poor')
+} else if(studentMarks === 0){
+    console.log('You are Fail')
+} else{
+    console.log('Your Marks need to be Calculated')
+}
