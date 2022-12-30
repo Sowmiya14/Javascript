49c166d19890022fccf3a7b61c7b3787658d28a9
Sowmiya14 <sowmiyak411@gmail.com>
2022-12-30 09:22:09 +0530

    SHIFT POP PUSH SPLICE

--- BASICS/02Basics/marvels.js
+++ BASICS/02Basics/marvels.js
@@ -16,3 +16,31 @@ console.log(`We have ${superHeroes.length} Super Heroes`);
 // const array =['Sowmi','Rini','Lavi','Bhuvi']
 // console.log(array[array.length -1]);
 // console.log(`Hey Hello We are ${array.length}`);
+
+
+
+
+
+
+// const numbers =['One','Two','Three','Four','Five','Six']
+// numbers[2] = 'Shifted'
+// console.log(numbers);
+//Start
+    // numbers.shift()
+    // console.log(numbers.shift());
+    // console.log(numbers);
+
+    // numbers.unshift('SOMETHING')
+    // console.log(numbers);
+
+//End
+
+    // numbers.pop()
+    // console.log('The Element to be Deleted is '+numbers.pop());
+    // console.log(numbers);
+    // numbers.push('Seven')
+    // console.log(numbers);
+
+//Middle
+    // numbers.splice(3,1,'NewOne')
+    // console.log(numbers);
